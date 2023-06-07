2871bc95f1379d3d4bee8aba0785f12b86eebd03
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-06-07 00:21:23 -0400

    add conn.mjs to connect to mongodb

---
+++ server/conn.mjs
@@ -0,0 +1,18 @@
+import { MongoClient } from "mongodb";
+
+const connectionString = process.env.ATLAS_URI || "";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+
+//try to connect to mongodb
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
+}
+
+let db = conn.db("match-journal-data");
+
+export default db;
--- server/server.mjs
+++ server/server.mjs
@@ -1,12 +1,12 @@
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 app.use("/records", records);
