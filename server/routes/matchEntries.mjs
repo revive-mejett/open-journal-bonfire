52cd2d7cccdbc79404d3c13f0099a81e7fdd54c4
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-06-08 23:08:55 -0400

    remove all API routes except for the GET all entries endpoint

--- server/routes/matchEntries.mjs
+++ server/routes/matchEntries.mjs
@@ -13,37 +13,5 @@ router.get("/", async (req, res) => {
   res.send(results).status(200); // send success response
 });
 
-// retrieve a specific match journal entry given by id as url path param
-router.get("/:id", async (req, res) => {
-  let collection = await db.collection(collectionName);
-  let query = {_id: new ObjectId(req.params.id)};
-  let result = await collection.findOne(query);
-
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200); // send success response
-});
-
-// create a new journal entry
-router.post("/", async (req, res) => {
-  let newDocument = {
-    name: req.body.name,
-    position: req.body.position,
-    level: req.body.level,
-  };
-  let collection = await db.collection(collectionName);
-  let result = await collection.insertOne(newDocument);
-  res.send(result).status(201); // send 201 response (success CREATED)
-});
-
-
-// deletes a match entry
-router.delete("/:id", async (req, res) => {
-    const query = { _id: new ObjectId(req.params.id) };
-  
-    const collection = db.collection(collectionName);
-    let result = await collection.deleteOne(query);
-  
-    res.send(result).status(200); //send success response
-  });
 
 export default router
