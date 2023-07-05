1d8df490f7e47a572cae3adb1458d15e45c80d88
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-07-05 14:34:42 -0400

    use match card component for each match entry data

--- client/src/pages/MatchEntriesPage.js
+++ client/src/pages/MatchEntriesPage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import MatchEntryCard from "../components/MatchEntryCard"
 
 const MatchEntriesPage = () => {
 
@@ -27,25 +28,7 @@ const MatchEntriesPage = () => {
     return (
         <>
             {matchEntryData &&
-                matchEntryData.map(entry => {
-                    return (
-                        <div>
-                            <h2>{entry.gameTitle}</h2>
-                            <p>outcome: {entry.isWon ? "WIN" : "LOSS"}</p>
-                            <div>
-                                {entry.teamReview.map(reviewPoint => <p>{reviewPoint}</p>)}
-                            </div>
-                            <div>
-                                {entry.teamPerformance.map(performanceItem => <p>{performanceItem}</p>)}
-                            </div>
-                            <div>
-                                {entry.events.map(eventItem => <p>{eventItem}</p>)}
-                            </div>
-                            entryContent: {entry.entryContent},
-                            <div>Rating: {entry.selfRating}</div>
-                        </div>
-                    )
-                })
+                matchEntryData.map(entry => <MatchEntryCard entry={entry}></MatchEntryCard>)
             }
         </>
     )
