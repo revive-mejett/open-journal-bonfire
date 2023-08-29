5ebb2fa53abe878d58c88f3962eee64fe7f2c698
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-08-28 21:48:22 -0400

    create placeholder link buttons in topbar

--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -1,8 +1,19 @@
+import { Link } from "react-router-dom"
 import "./Topbar.scss"
 
 const Topbar = () => {
     return (
-        <div className="topbar-sample">topbar</div>
+        <div className="topbar-sample">
+            <div>
+                <Link to="/">View All</Link>
+            </div>
+            <div>
+                <Link to="/some">Random Entry</Link>
+            </div>
+            <div>
+                <Link to="/new">Create new Anonymous Entry</Link>
+            </div>
+        </div>
     )
 }
 
