eb4145fece6f586f07362a0c627e7aff944f8526
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-07 14:41:33 -0400

    checkbox that adds to journal entry's good events

--- client/src/pages/CreateEntryPage.js
+++ client/src/pages/CreateEntryPage.js
@@ -1,16 +1,30 @@
 import { Field, Formik } from "formik"
+import { useState } from "react"
 
 const CreateEntryPage = () => {
+
+    //initial good events
+    const possibleGoodEventsInitial = ["Had a nice dinner", "Had a nice outing", "did well on a test", "got a partner"]
+
+    const [goodEvents, setGoodEvents] = useState([])
+    const [possibleGoodEvents, setGoodEventBank] = useState(possibleGoodEventsInitial)
 
     const handleSubmit = (values) => {
         alert("test form submit")
         console.log(values)
     }
 
+    const handleGoodEventsChange = (e) => {
+        console.log(e.target.value)
+        goodEvents.push(e.target.value)
+        let updatedPossibleGoodEvents = [...possibleGoodEvents]
+        updatedPossibleGoodEvents = updatedPossibleGoodEvents.filter(goodEvent => goodEvent !== e.target.value)
+        setGoodEventBank(updatedPossibleGoodEvents)
+        setGoodEvents(goodEvents)
+        console.log(goodEvents)
 
-    const handleGoodEventsChange = (e) => console.log(e.target)
+    }
 
-    const possibleGoodEvents = ["Had a nice dinner", "Had a nice outing", "did well on a test", "got a partner"]
     const possibleNeutralEvents = ["nothing too special", "normal day at work", "normal day at school"]
     const possibleWorseEvents = ["failed a test in school", "thundering", "lost a team sports game", "had a breakup"]
 
@@ -23,7 +37,7 @@ const CreateEntryPage = () => {
                     onSubmit={handleSubmit}
                 >
                     {props => (
-                        <form onSubmit={props.handleSubmit}>
+                        <form onSubmit={props.handleSubmit} onChange={handleGoodEventsChange}>
                             <input type="text" name="title" />
                             {/* <Field as="select" name="goodEventsList" onChange={handleGoodEventsChange} multiple>
                                 {possibleGoodEvents.map((event, index) => <option value={event} onInput={() => alert("fied")} key={index}>{event}</option>)}
