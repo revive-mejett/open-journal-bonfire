d786eb20a6cb5e836af5284c0ab23d15e244718b
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-14 13:49:14 -0400

    Integrate fetching the api GET route to fetch info of an entry

--- client/src/pages/JournalEntryDetail.js
+++ client/src/pages/JournalEntryDetail.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react"
 import { useLocation } from "react-router-dom"
 
 const JournalEntryDetail = () => {
 
     const location = useLocation()
+    
+    const [journalEntryData, setJournalEntryData] = useState(undefined)
+
+    useEffect(() => {
+        (async () => {
+            let urlParams = new URLSearchParams(location.search)
+            let searchId = urlParams.get("id")
+
+            let data
+            if (!journalEntryData) {
+                let response = await fetch("/api/journalentries/" + searchId)
+
+                if (response.ok) {
+                    console.log("response success -- " + response.status)
+                } else {
+                    console.error("response not ok -- " + response.status)
+                }
+                data = await response.json()
+                setJournalEntryData(data)
+            }
+        })();
+        console.log(journalEntryData)
+    })
+
     return (
         <h2>Journal entry detail</h2>
     )
