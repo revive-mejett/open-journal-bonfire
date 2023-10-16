164fc1752a5e87921abc89e3d77246142f9bb5f7
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-16 18:12:27 -0400

    add form input field styles

--- client/src/pages/CreateEntryPage.js
+++ client/src/pages/CreateEntryPage.js
@@ -1,5 +1,7 @@
 import { Field, Formik } from "formik"
 import EventKeywordPicker from "../components/forms/EventKeywordPicker"
+import "../assets/forminputstyle.scss"
+import "./CreateEntryPage.scss"
 
 const CreateEntryPage = () => {
 
@@ -13,7 +15,7 @@ const CreateEntryPage = () => {
     //initial not so good events
     const possibleWorseEvents = ["failed a test in school", "thundering", "lost a team sports game", "had a breakup"]
 
-    const selfRatingValues = [1,2,3,4,5,6,7,8,9,10]
+    const selfRatingValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     const handleSubmit = async (values) => {
         let data = {
@@ -54,16 +56,22 @@ const CreateEntryPage = () => {
             <div>
                 <h2>Create an Anonymous Journal Entry</h2>
                 <Formik
-                    initialValues={{ title: "Untitled", entryContent: "", goodEventsList: [], neutralEventsList: [], worseEventsList: [], goodEventsBank: possibleGoodEventsInitial, selfRating: 0}}
+                    initialValues={{ title: "Untitled", entryContent: "", goodEventsList: [], neutralEventsList: [], worseEventsList: [], goodEventsBank: possibleGoodEventsInitial, selfRating: 0 }}
                     onSubmit={handleSubmit}
                 >
                     {props => (
                         <form onSubmit={props.handleSubmit}>
-                            <label htmlFor="title" placeholder="Another day to say...">Title of your entry:</label>
-                            <Field name="title"></Field>
 
-                            <label htmlFor="entryContent">Your entry...</label>
-                            <Field name="entryContent" as="textarea" placeholder="// Write away..."></Field>
+                            <div className="form-section">
+                                <label htmlFor="title" placeholder="Another day to say...">Title of your entry:</label>
+                                <Field name="title" className="input-text-field entry-title-field"></Field>
+                            </div>
+
+                            <div className="form-section">
+                                <label htmlFor="entryContent">Your entry...</label>
+                                <Field name="entryContent" as="textarea" placeholder="// Write away..." className="input-text-field entry-content-textarea"></Field>
+                            </div>
+
 
                             <EventKeywordPicker
                                 eventBank={possibleGoodEventsInitial}
