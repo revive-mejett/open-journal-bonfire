2be9d90984b4a74ff5b4e0032321136a6984b7e3
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-21 23:52:50 -0400

    add min and max self rating input sliders

--- client/src/pages/JournalEntriesPage.js
+++ client/src/pages/JournalEntriesPage.js
@@ -6,11 +6,6 @@ import { Field, Formik } from "formik"
 const JournalEntriesPage = () => {
 
     const [entryData, setEntryData] = useState(undefined)
-    const selfRatingFilterLabel = useRef()
-
-    const updateSelfRatingLabel = e => {
-        selfRatingFilterLabel.current.textContent = e.target.value
-    }
 
     const handleSubmit = e => {
         console.log("Apply filter button not implemented yet.")
@@ -41,16 +36,23 @@ const JournalEntriesPage = () => {
         <main className="jounal-entries-page-main">
             <section className="filters-container">
                 <Formik
-                    initialValues={{ titleFilterMatch: "", entryContentMatch: "", selfRatingFilter: 5 }}
+                    initialValues={{ titleFilterMatch: "", entryContentMatch: "", minSelfRating: 1, maxSelfRating: 10 }}
                     onSubmit={handleSubmit}
                 >
                     {props => (<form onSubmit={props.handleSubmit}>
                         <label htmlFor="titleFilterMatch">Filter title containing:</label>
                         <Field as="input" name="titleFilterMatch" className="input-text-field"></Field>
-                        <label htmlFor="entryContentMatch">Filter title containing:</label>
+
+                        <label htmlFor="entryContentMatch">Filter entry text containing:</label>
                         <Field as="input" name="entryContentMatch" className="input-text-field"></Field>
-                        <label htmlFor="selfRatingFilter">Minimum self-rating:</label>
-                        <Field as="input" type="range" name="selfRatingFilter" step="1" min="1" max="10" onInput={updateSelfRatingLabel}></Field><span ref={selfRatingFilterLabel}>11</span>
+
+                        
+                        <label htmlFor="minSelfRating">Minimum self-rating:</label>
+                        <Field as="input" type="range" name="minSelfRating" step="1" min="1" max="10"></Field><span>{props.values.minSelfRating}   </span>
+
+                        <label htmlFor="maxSelfRating">Maximum self-rating:</label>
+                        <Field as="input" type="range" name="maxSelfRating" step="1" min="1" max="10"></Field><span>{props.values.maxSelfRating}   </span>
+
                         <button type="submit" className="button">Apply Filters</button>
                     </form>)}
                 </Formik>
