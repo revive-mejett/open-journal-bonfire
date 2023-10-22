d09496b822b48825afab1a986bd87474a83dbffe
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-21 23:22:01 -0400

    add Formik form to contain fields used to filter entries containing matching text

--- client/src/App.js
+++ client/src/App.js
@@ -7,16 +7,16 @@ import CreateEntryPage from "./pages/CreateEntryPage";
 import JournalEntryDetail from "./pages/JournalEntryDetail";
 
 const App = () => {
- return (
-   <div>
-    <Topbar/>
-     <Routes>
-       <Route exact path="/" element={<JournalEntriesPage/>}/>
-       <Route exact path="/entries/new" element={<CreateEntryPage/>}/>
-       <Route exact path="/entries/viewing" element={<JournalEntryDetail/>}/>
-     </Routes>
-   </div>
- );
+    return (
+        <>
+            <Topbar />
+            <Routes>
+                <Route exact path="/" element={<JournalEntriesPage />} />
+                <Route exact path="/entries/new" element={<CreateEntryPage />} />
+                <Route exact path="/entries/viewing" element={<JournalEntryDetail />} />
+            </Routes>
+        </>
+    );
 };
- 
+
 export default App;
--- client/src/pages/JournalEntriesPage.js
+++ client/src/pages/JournalEntriesPage.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react"
 import JournalEntryCard from "../components/JournalEntryCard"
 import "./JournalEntriesPage.scss"
+import { Field, Formik } from "formik"
 
 const JournalEntriesPage = () => {
 
     const [entryData, setEntryData] = useState(undefined)
 
-    useEffect(()=> {
-        
+    const handleSubmit = (e) => {
+        console.log("Apply filter button not implemented yet.")
+    }
+
+
+    useEffect(() => {
+
         const fetchEntryData = async () => {
             try {
                 let response = await fetch("/api/journalentries")
@@ -17,7 +23,7 @@ const JournalEntriesPage = () => {
                     let data = await response.json()
                     setEntryData(data)
                 }
-            } catch(error) {
+            } catch (error) {
                 console.error("Error fetching match data --> ", error)
             }
         }
@@ -28,12 +34,26 @@ const JournalEntriesPage = () => {
 
     return (
         <main className="jounal-entries-page-main">
-            <section className="entries-container">
-            {entryData &&
-                entryData.map((entry,i) => <JournalEntryCard key={i} entry={entry}></JournalEntryCard>)
-            }
+            <section className="filters-container">
+                <Formik
+                    initialValues={{ titleFilterMatch: "", entryContentMatch: "", selfRatingFilter: 0 }}
+                    onSubmit={handleSubmit}
+                >
+                    {props => (<form onSubmit={props.handleSubmit}>
+                        <label htmlFor="titleFilterMatch">Filter title containing:</label>
+                        <Field as="input" name="titleFilterMatch" className="input-text-field"></Field>
+                        <label htmlFor="entryContentMatch">Filter title containing:</label>
+                        <Field as="input" name="entryContentMatch" className="input-text-field"></Field>
+                        <button type="submit" className="button">Apply Filters</button>
+                    </form>)}
+                </Formik>
             </section>
-            
+            <section className="entries-container">
+                {entryData &&
+                    entryData.map((entry, i) => <JournalEntryCard key={i} entry={entry}></JournalEntryCard>)
+                }
+            </section>
+
         </main>
     )
 }
