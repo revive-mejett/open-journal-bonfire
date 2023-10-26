a6ac8859b40a696e7a1e7d3cde073310865bdf5c
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-26 14:27:26 -0400

    instead of type, define a magnitude value from -10 to 10, where 10 is most positive

--- server/models/EventTag.js
+++ server/models/EventTag.js
@@ -2,17 +2,25 @@ import mongoose from "mongoose";
 const { Schema, model } = mongoose;
 //object destructuring
 
+const magnitudeValidator = (value) => {
+    return value >= -10 && value <= 10
+}
 
 const frequentEventTagSchema = new Schema({
-    keyword: {type: String, required: true, maxLength: 50},
-    type: {type: String, required: true, default: "neutral"},
-    weight: {type: Number, required: true, default: 5},
+    keyword: {type: String, required: true, maxLength: 50, immutable: true},
+    magnitude: {type: Number, required: true, default: 0, validate: magnitudeValidator, immutable: true},
+    weight: {type: Number, required: true, default: 0, immutable: true},
     lastUsed: {type: Date, default: Date.now},
-    dateCreated: {type: Date, default: Date.now},
-    permanent: {type: Boolean, default: false},
+    dateCreated: {type: String, immutable: true, default: Date.now},
+    permanent: {type: Boolean, default: false, immutable: true},
     frequency: {type: Number, default: 1}
 })
 
-const JournalEntry = model("FrequentEventTag", frequentEventTagSchema)
+frequentEventTagSchema.pre("save", (next) => {
+    this.weight = 5 ** Math.abs(this.magnitude) * (this.magnitude > 0 ? 1 : -1)
+    next()
+})
 
-export default JournalEntry
+const frequentEventTag = model("FrequentEventTag", frequentEventTagSchema)
+
+export default frequentEventTag
