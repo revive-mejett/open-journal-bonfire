92c52cdcc5829694722e4722c0b222d0e00c834c
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-26 22:53:05 -0400

    remove hard-coded temporary event tag lists

--- client/src/pages/CreateEntryPage.js
+++ client/src/pages/CreateEntryPage.js
@@ -3,31 +3,15 @@ import EventKeywordPicker from "../components/forms/EventKeywordPicker"
 import "../assets/forminputstyle.scss"
 import "./CreateEntryPage.scss"
 import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 const CreateEntryPage = () => {
 
-    //Note: temporary list - it will later be stored in a mongodb collection (maybe)
-    //initial good events 
-    const possibleGoodEventsInitial = ["Had a nice dinner", "Had a nice outing", "did well on a test", "got a partner", "bought a car", "bought a house", "got married", 
-        "got a job", "got an interview", "got an offer", "aced my exam", "got my licence", "bought a new pc", "went on a trip", "today's my birthday", "my friend's birthday", "my girlfriend's birthday",
-        "my boyfriend's birthday", "bought a new game", "got promoted in league of legends", "passed math", "passed history", "passed science", "got a new baby", "good haircut", "went on nice restaurant",
-        "got robux", "got vbucks", "gained new subs on youtube", "great day streaming", "got promoted", "got a raise", "got new benefits", "got a boyfriend", "got a girlfriend", "had a crush",
-        "moving on to next semester", "found an new game on steam", "passed french", "went to the beach", "new laptop", "new keyboard", "new gaming chair", "new mouse", "new table", "new jewelry",
-        "business making profit"
-        ]
+    const [frequentKeywordData, setFrequentKeywordData] = useState({})
 
-    //initial okay/neutral events
-    const possibleNeutralEvents = ["nothing too special", "normal day at work", "normal day at school", "ate a decent meal", "took a test", "got some homework", "slept ok", "busy day at school",
-            "busy day at work", "had an interview", "made errands", "took a walk", "exercised", "did chores", "cleaned the house", "did laundry", "did some homework", "cloudy day", "overcast",
-            "went shopping", "went somewhere", "bought something necessary"
-        ]
+    useEffect(() => {
 
-    //initial not so good events
-    const possibleWorseEvents = ["failed a test in school", "thundering", "lost a team sports game", "had a breakup", "got demoted", "got fired", "got written up", "placed on a PIP", "got fired", "got laid off",
-        "failed a final", "failed a course", "failed my driving test", "broke something", "had a fight with my parents", "had a fight with my partner", "got sick", "got a cold", "got covid", "injury",
-        "failed math", "failed history", "failed french", "got held back", "car broke down", "car got vandalized", "flat tire", "car got git", "had a meal I didint like", "hurt myself by accident", "boyfriend dumped me",
-        "girlfriend dumped me"
-    ]
+    })
 
     const selfRatingValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
