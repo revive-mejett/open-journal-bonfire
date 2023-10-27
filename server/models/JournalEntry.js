8133a88ff02cf79ba07172b8b28c65d1576df115
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-27 12:11:08 -0400

    update journal entry schema to contain more detailed event tags

--- server/models/JournalEntry.js
+++ server/models/JournalEntry.js
@@ -7,9 +7,21 @@ const entrySchema = new Schema({
     title: {type: String, required: true},
     entryContent: {type: String, trim: true, maxLength: 7000},
     selfRating: {type: Number, min: 1, max: 10},
-    greatEvents: {type: [String], default: []},
-    neutralEvents: {type: [String], default: []},
-    badEvents: {type: [String], default: []},
+    greatEvents: [{
+        keyword: {type: String},
+        magnitude: {type: Number},
+        weight: {type: Number},
+    }],
+    neutralEvents: [{
+        keyword: {type: String},
+        magnitude: {type: Number},
+        weight: {type: Number},
+    }],
+    badEvents: [{
+        keyword: {type: String},
+        magnitude: {type: Number},
+        weight: {type: Number},
+    }],
     flairs: {type: [String], default: []},   
     // keywords which will be displayed on a card (ex: birthday, passed exam, had breakup)
     dateCreated: {type: Date, default: Date.now}
