bd42307ea0ebd84a46ba4c1e5f88175b2ec61d02
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-10-29 15:50:53 -0400

    add grid css to some sections

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -9,6 +9,8 @@ const Homepage = () => {
 
     const numberSampleEntries = 3
 
+    const addParticle = () => <div className="particle"></div>
+
     useEffect(() => {
         const fetchSampleEntries = async () => {
             try {
@@ -29,15 +31,24 @@ const Homepage = () => {
     }, [sampleEntries])
 
     return (
-        <main>
-            <h1>
-                an Open Journal Bonfire! 🔥
-            </h1>
-            <section>
+        <main className="homepage-main">
+            <header className="presentation-section introduction-heading">
+                <h1>
+                    an Open Journal Bonfire! 🔥
+                </h1>
+                <p>A free space for everyone to write and freely share their days what they want. No trees harmed!</p>
+                <div className="presentation-visual fire-container-introsection">
+                    {new Array(15).fill(addParticle())}
+                </div>
+            </header>
+
+            <section className="presentation-section creation-feature-presentation">
                 <h2>
                     Take a pencil (your keyboard) and a piece of paper (your screen). Write your day..
                 </h2>
                 <p>Did you pass your science test? Share it! Got a new car? Jot what model and colour you got! Got an unfortunate bruise? Ouch. Explain how did you get that little bruise. Share all your amazing experience you have had today! Or dump it all! Write freely and anonymously!</p>
+                <div className="presentation-visual">
+                </div>
                 <div className="link-container">
                     <Link to="/entries/new" className="link-button">Create now</Link>
                 </div>
