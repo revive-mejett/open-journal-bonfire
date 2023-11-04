d0fb787b175336b4ef8702c27a0d69fbdeb0e7e0
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-11-03 21:42:28 -0400

    rename to MatchEntryCard.scss

--- client/src/components/JournalEntryCard.js
+++ client/src/components/JournalEntryCard.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import "./MatchEntryCard.scss"
+import "./JournalEntryCard.scss"
 
 const JournalEntryCard = ({ entry }) => {
 
