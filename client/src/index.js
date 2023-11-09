c09cf32bec6f3ef9f376ba94966e3f7b84cbc9ff
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-11-09 12:42:07 -0500

    add validation for title, entrycontent, event tag limit

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
--- client/src/pages/CreateEntryPage.js
+++ client/src/pages/CreateEntryPage.js
@@ -5,11 +5,30 @@ import "./CreateEntryPage.scss"
 import { useNavigate } from "react-router-dom"
 import { useEffect, useState } from "react"
 import Background from "../components/visuals/Background"
+import * as Yup from "yup"
 
 const CreateEntryPage = () => {
 
     const [frequentKeywordData, setFrequentKeywordData] = useState(undefined)
 
+    const journalEntrySchema = Yup.object().shape({
+        title: Yup.string()
+            .max(50, "Title is too long"),
+        entryContent: Yup.string()
+            .min(20, "Too short!")
+            .max(7000, "Entry is too long")
+            .required("Journal entries cannot be empty!"),
+        goodEventsList: Yup.array().of(Yup.object()).min(0),
+        neutralEventsList: Yup.array().of(Yup.object()).min(0),
+        worseEventsList: Yup.array().of(Yup.object()).min(0)
+        .test({
+            name: "maxTags",
+            test: function(value) {
+                return (this.parent.goodEventsList.length + this.parent.neutralEventsList.length + this.parent.worseEventsList.length) <= 10
+            },
+            message: "Maximum limit of 10 tags reached"
+        })
+    })
     useEffect(() => {
 
         (async () => {
@@ -19,7 +38,6 @@ const CreateEntryPage = () => {
                     console.error("response not ok")
                 } else {
                     let data = await response.json()
-                    console.log(data)
                     setFrequentKeywordData(data)
                 }
             }
@@ -87,19 +105,23 @@ const CreateEntryPage = () => {
                 <h2>Create an Anonymous Journal Entry</h2>
                 <Formik
                     initialValues={{ title: "Untitled", entryContent: "", goodEventsList: [], neutralEventsList: [], worseEventsList: [], selfRating: 0, eventTagTextFilter : "" }}
+                    validationSchema={journalEntrySchema}
                     onSubmit={handleSubmit}
                 >
                     {props => (
                         <form onSubmit={props.handleSubmit}>
-
+                            
                             <div className="form-section">
+                                
                                 <label htmlFor="title" placeholder="Another day to say...">Title of your entry:</label>
                                 <Field name="title" className="input-text-field entry-title-field"></Field>
+                                {props.errors.title ? <span className="error">   {props.errors.title}</span> : null}
                             </div>
 
                             <div className="form-section">
                                 <label htmlFor="entryContent">Your entry...</label>
                                 <Field name="entryContent" as="textarea" placeholder="// Write away..." className="input-text-field entry-content-textarea"></Field>
+                                {props.errors.entryContent ? <p className="error">   {props.errors.entryContent}</p> : null}
                             </div>
 
                             <div className="form-section">
@@ -107,7 +129,7 @@ const CreateEntryPage = () => {
                                 <Field name="eventTagTextFilter" className="input-text-field filter-tags-field"></Field>
                             </div>
                             
-
+                            {props.errors.worseEventsList ? <span className="error">{props.errors.worseEventsList}</span> : null}
                             <fieldset className="event-tags-subforms-container form-section">
                                 
                                 <div className="subform-flex-item">
@@ -157,9 +179,10 @@ const CreateEntryPage = () => {
                             </fieldset>
 
                             <div className="form-section self-rating-form">
-                                <label htmlFor="selfRating">Out of 10, rate your day:</label>
+                                <label htmlFor="selfRating">Rate your day from -10 (absolute worst) to 10 (absolute best)</label>
+                                <p>Note: 0 is neutral</p>
                                 <Field as="select" name="selfRating">
-                                    <option value={0} hidden>--select--</option>
+                                    <option value={0} hidden>0</option>
                                     {selfRatingValues.map(ratingValue => <option value={ratingValue} key={ratingValue}>{ratingValue}</option>)}
                                 </Field>
                             </div>
