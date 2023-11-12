cced812b0a57521dc0d8bbf9c0084123d3e0f31b
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-11-12 14:32:03 -0500

    add page, link route and add topbar route to statistic bonfire page

---
+++ client/src/pages/BonfireStatistics.js
@@ -0,0 +1,9 @@
+const BonfireStatistics = () => {
+    return (
+        <main className="bonfire-stats-page-main">
+            <h1>Bonfire statistics page</h1>
+        </main>
+    )
+}
+
+export default BonfireStatistics
