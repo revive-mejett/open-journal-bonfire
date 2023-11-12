97e4099e3dd338a1b061e18ff61ef83effc54e3e
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-11-12 12:46:59 -0500

    readd react strict mode for development

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,10 @@ import { createRoot } from 'react-dom/client';
 const container = document.getElementById("root")
 const root = createRoot(container)
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </React.StrictMode>,
+  </React.StrictMode>,
   
 );
