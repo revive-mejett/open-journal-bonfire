86969484eb32cf8cd955eec1c268f4d094682285
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-12-17 11:13:29 -0500

    serve using client-side routing

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ app.use(express.json());
 app.use("/api", records);
 
 
+const BUILD_DIR = "client/build"
+app.use(express.static(BUILD_DIR))
+// Redirect to index.html for the react router to handle
+app.get('*', (req, res) => res.sendFile(path.resolve(BUILD_DIR, 'index.html')));
 
 
 export default app;
